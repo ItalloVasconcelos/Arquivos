f8140c279305151d779a876a4226efcc8ed00857
Itallo Vasconcelos <itallo.prog@gmail.com>
2022-04-04 11:20:06 -0300

    feat: create and organized pages and partials

--- server.js
+++ server.js
@@ -6,13 +6,38 @@ app.set("view engine", "ejs")
 
 
 app.get("/", function (req, res){
-    res.render("index");
+    const items = [
+        {
+            title: "D",
+            message: "Desenvolvimento de aplicações/serviços de forma ágil"
+        },
+        {
+            title: "E",
+            message: "EJS usa Javascript para renderizar o HTML"
+        },
+        {
+            title: "M",
+            message: "Muito mais fácil de utilizar"
+        },
+        {
+            title: "A",
+            message: "Arroba: itallodev"
+        }
+    ];
+
+
+    const subtitle = "Uma linguagem de modelagem para criação de páginas HTML"
+    res.render("pages/index", {
+        qualitys: items,
+        subtitle: subtitle,
+    });
 })
 
+
 app.get("/sobre", function (req, res){
-    res.render("about");
+    res.render("pages/about");
 })
 
 app.listen(8080)
 
-console.log("Rodando!")
+console.log("Servidor Online!")
